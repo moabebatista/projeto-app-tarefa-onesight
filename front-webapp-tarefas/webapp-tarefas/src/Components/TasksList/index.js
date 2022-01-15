f2bf448b138b041af08753c37418da022a52a05c
Moabe Batista <moabebatista@gmail.com>
2022-01-15 17:47:40 -0300

    listando tarefas com dados mocados

--- front-webapp-tarefas/webapp-tarefas/src/Components/TasksList/index.js
+++ front-webapp-tarefas/webapp-tarefas/src/Components/TasksList/index.js
@@ -1,10 +1,32 @@
 import './styles.css';
-import TableHeader from './TableHeader'
+import TableHeader from './TableHeader';
+import deleteIcon from '../../assets/delete-icon.svg';
+import editIcon from '../../assets/edit-icon.svg'
 
 function TasksList() {
     return (
-        <div>
+        <div className="table">
             <TableHeader />
+            <div className="table-body">
+                <div className="table-line">
+                    <div className="line-itens">
+                        01/02/2022   
+                    </div>
+                    <div className="line-itens">
+                        Terça   
+                    </div>
+                    <div className="line-itens">
+                        Lavar o carro   
+                    </div>
+                    <div className="line-itens">
+                        Fazer   
+                    </div>
+                    <div className="line-itens cursor-pointer">
+                        <img src={editIcon} alt="edit icon" />
+                        <img src={deleteIcon} alt="delete icon" />   
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
