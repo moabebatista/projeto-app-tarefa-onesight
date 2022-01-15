264251b3feab5628d58fd317fc9455e7afa138a5
Moabe Batista <moabebatista@gmail.com>
2022-01-15 13:08:38 -0300

    criando o controlador e a rota get para listar as tarefas

--- back-api-tarefas/src/controllers/TasksController.js
+++ back-api-tarefas/src/controllers/TasksController.js
@@ -1,7 +1,16 @@
 const api = require('../services/api');
 
 class TasksController {
-
+    async listAll(req, res) {
+        try {
+          const response = await api.get('/tasks');
+    
+          return res.status(200).json(response.data);
+    
+        } catch (error) {
+          return res.status(400).json("Falha ao listar as tarefa.");
+        }
+      }
 }
 
 module.exports = new TasksController();
--- back-api-tarefas/src/routes.js
+++ back-api-tarefas/src/routes.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
+const TasksController = require("./controllers/TasksController");
 
 const routes = Router();
+
+routes.get('/tasks', TasksController.listAll);
 
 module.exports = routes;
