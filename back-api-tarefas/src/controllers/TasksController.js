cc421bcd868ea786759c8fb07377ac3858a62531
Moabe Batista <moabebatista@gmail.com>
2022-01-15 13:19:41 -0300

    criando controlador create que permite ao usuario criar novas tarefas

--- back-api-tarefas/src/controllers/TasksController.js
+++ back-api-tarefas/src/controllers/TasksController.js
@@ -11,6 +11,20 @@ class TasksController {
           return res.status(400).json("Falha ao listar as tarefa.");
         }
       }
+
+      async create(req, res) {
+        try {
+          const { body } = req;
+    
+          const response = await api.post('/tasks', {
+            ...body
+          });
+    
+          return res.status(200).json(response.data);
+        } catch (error) {
+          return res.status(400).json("Falha ao criar uma tarefa");
+        }
+      }
 }
 
 module.exports = new TasksController();
